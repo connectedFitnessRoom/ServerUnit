3ac913fabf46f62d06ecadba74b0ed67da0a6fb2
Xantin <xantin@live.be>
2024-12-06 11:42:26 +0100

    Database not in memory

--- build.sbt
+++ build.sbt
@@ -2,9 +2,11 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.3.4"
 
-// Adding Slick to project
+// Adding Slick to project )
+
 libraryDependencies ++= Seq(
   "com.typesafe.slick" %% "slick" % "3.5.2",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.5.2", // HikariCP for connection pooling
   "org.slf4j" % "slf4j-nop" % "2.0.16",
   "com.h2database" % "h2" % "2.3.232"
 )
--- src/main/scala/be/serverunit/database/utils/InitDatabase.scala
+++ src/main/scala/be/serverunit/database/utils/InitDatabase.scala
@@ -3,6 +3,7 @@ package be.serverunit.database.utils
 import be.serverunit.database.{Air, Machine, Repetition, Set, SlickTables, User, UserSession}
 import slick.jdbc.H2Profile.api.*
 import slick.jdbc.JdbcBackend.Database
+import slick.jdbc.meta.MTable
 
 import java.time.Instant
 import java.time.temporal.ChronoUnit
@@ -11,72 +12,77 @@ import scala.util.{Failure, Success}
 
 object InitDatabase {
   def setupDatabase(db: Database)(implicit ec: ExecutionContext): Future[Unit] = {
-    val setup = for {
-      // Create the tables, including primary and foreign keys
-      _ <- db.run((SlickTables.users.schema ++ SlickTables.sessions.schema ++ SlickTables.sets.schema ++ SlickTables.machines.schema ++ SlickTables.repetitions.schema ++ SlickTables.airs.schema).create)
+    val tables = List(
+      SlickTables.users,
+      SlickTables.sessions,
+      SlickTables.sets,
+      SlickTables.machines,
+      SlickTables.repetitions,
+      SlickTables.airs
+    )
 
-      // Insert some users
-      _ <- db.run(DBIO.seq(
-        SlickTables.users += User("1", "Alice", "password"),
-        SlickTables.users += User("2", "Bob", "password"),
-        SlickTables.users += User("3", "Charlie", "password")
-      ))
+    val existingTables = db.run(MTable.getTables).map(_.map(_.name.name).toSet)
 
-      // Insert some sessions and retrieve the auto-generated SESSION_IDs
-      sessionIds <- db.run(DBIO.seq(
-        SlickTables.sessions += UserSession(0, "1", Instant.parse("2024-12-01T00:00:00Z"), None),
-        SlickTables.sessions += UserSession(0, "1", Instant.parse("2024-12-02T00:00:00Z"), None),
-        SlickTables.sessions += UserSession(0, "1", Instant.parse("2024-12-03T00:00:00Z"), None),
-        SlickTables.sessions += UserSession(0, "2", Instant.now(), None),
-        SlickTables.sessions += UserSession(0, "3", Instant.now(), None),
-        // New sessions for next week
-        SlickTables.sessions += UserSession(0, "1", Instant.now().plus(7, ChronoUnit.DAYS), None),
-        SlickTables.sessions += UserSession(0, "1", Instant.now().plus(8, ChronoUnit.DAYS), None),
-        SlickTables.sessions += UserSession(0, "1", Instant.now().plus(9, ChronoUnit.DAYS), None)
-      ).flatMap(_ => SlickTables.sessions.result))
+    existingTables.flatMap { existing =>
+      val createIfNotExists = tables.filterNot(table => existing.contains(table.baseTableRow.tableName)).map(_.schema.createIfNotExists)
+      val setup = DBIO.sequence(createIfNotExists).transactionally
 
-      // Insert machines
-      _ <- db.run(DBIO.seq(
-        SlickTables.machines += Machine(0, "Machine 1"),
-        SlickTables.machines += Machine(1, "Machine 2"),
-        SlickTables.machines += Machine(2, "Machine 3")
-      ))
+      db.run(setup).flatMap { _ =>
+        if (existing.isEmpty) {
+          val insertData = DBIO.seq(
+            // Insert some users
+            SlickTables.users += User("1", "Alice", "password"),
+            SlickTables.users += User("2", "Bob", "password"),
+            SlickTables.users += User("3", "Charlie", "password"),
 
-      // Insert sets using the correct sessionIds
-      _ <- db.run(DBIO.seq(
-        // Sets for user 1 with different durations
-        SlickTables.sets += Set(0, sessionIds.head.id, 0, Instant.parse("2024-12-02T00:00:00Z"), Some(Instant.parse("2024-12-02T01:00:00Z")), None, 0),
-        SlickTables.sets += Set(0, sessionIds(1).id, 0, Instant.parse("2024-12-03T00:00:00Z"), Some(Instant.parse("2024-12-03T02:00:00Z")), None, 0),
-        SlickTables.sets += Set(0, sessionIds(2).id, 0, Instant.parse("2024-12-04T00:00:00Z"), Some(Instant.parse("2024-12-04T03:00:00Z")), None, 0),
-        // New sets for next week
-        SlickTables.sets += Set(0, sessionIds(5).id, 0, Instant.now().plus(7, ChronoUnit.DAYS), Some(Instant.now().plus(7, ChronoUnit.DAYS).plus(1, ChronoUnit.HOURS)), None, 0),
-        SlickTables.sets += Set(0, sessionIds(6).id, 0, Instant.now().plus(8, ChronoUnit.DAYS), Some(Instant.now().plus(8, ChronoUnit.DAYS).plus(2, ChronoUnit.HOURS)), None, 0),
-        SlickTables.sets += Set(0, sessionIds(7).id, 0, Instant.now().plus(9, ChronoUnit.DAYS), Some(Instant.now().plus(9, ChronoUnit.DAYS).plus(3, ChronoUnit.HOURS)), None, 0),
+            // Insert some sessions and retrieve the auto-generated SESSION_IDs
+            SlickTables.sessions += UserSession(0, "1", Instant.parse("2024-12-01T00:00:00Z"), None),
+            SlickTables.sessions += UserSession(0, "1", Instant.parse("2024-12-02T00:00:00Z"), None),
+            SlickTables.sessions += UserSession(0, "1", Instant.parse("2024-12-03T00:00:00Z"), None),
+            SlickTables.sessions += UserSession(0, "2", Instant.now(), None),
+            SlickTables.sessions += UserSession(0, "3", Instant.now(), None),
+            // New sessions for next week
+            SlickTables.sessions += UserSession(0, "1", Instant.now().plus(7, ChronoUnit.DAYS), None),
+            SlickTables.sessions += UserSession(0, "1", Instant.now().plus(8, ChronoUnit.DAYS), None),
+            SlickTables.sessions += UserSession(0, "1", Instant.now().plus(9, ChronoUnit.DAYS), None),
 
-        // Sets for other users
-        SlickTables.sets += Set(1, sessionIds(3).id, 1, Instant.now(), None, None, 0),
-        SlickTables.sets += Set(2, sessionIds(4).id, 2, Instant.now(), None, None, 0)
-      ))
+            // Insert machines
+            SlickTables.machines += Machine(0, "Machine 1"),
+            SlickTables.machines += Machine(1, "Machine 2"),
+            SlickTables.machines += Machine(2, "Machine 3"),
 
-      // Insert some repetitions
-      _ <- db.run(DBIO.seq(
-        SlickTables.repetitions += Repetition(1, 0, 0),
-        SlickTables.repetitions += Repetition(2, 0, 0),
-        SlickTables.repetitions += Repetition(3, 0, 0)
-      ))
+            // Insert sets using the correct sessionIds
+            SlickTables.sets += Set(0, 1, 0, Instant.parse("2024-12-02T00:00:00Z"), Some(Instant.parse("2024-12-02T01:00:00Z")), None, 0),
+            SlickTables.sets += Set(0, 2, 0, Instant.parse("2024-12-03T00:00:00Z"), Some(Instant.parse("2024-12-03T02:00:00Z")), None, 0),
+            SlickTables.sets += Set(0, 3, 0, Instant.parse("2024-12-04T00:00:00Z"), Some(Instant.parse("2024-12-04T03:00:00Z")), None, 0),
+            // New sets for next week
+            SlickTables.sets += Set(0, 6, 0, Instant.now().plus(7, ChronoUnit.DAYS), Some(Instant.now().plus(7, ChronoUnit.DAYS).plus(1, ChronoUnit.HOURS)), None, 0),
+            SlickTables.sets += Set(0, 7, 0, Instant.now().plus(8, ChronoUnit.DAYS), Some(Instant.now().plus(8, ChronoUnit.DAYS).plus(2, ChronoUnit.HOURS)), None, 0),
+            SlickTables.sets += Set(0, 8, 0, Instant.now().plus(9, ChronoUnit.DAYS), Some(Instant.now().plus(9, ChronoUnit.DAYS).plus(3, ChronoUnit.HOURS)), None, 0),
 
-      // Insert some air qualities
-      _ <- db.run(DBIO.seq(
-        SlickTables.airs += Air(0, 0, 0, 0, Instant.now()),
-        SlickTables.airs += Air(1, 3, 4, 5, Instant.now()),
-        SlickTables.airs += Air(2, 6, 7, 8, Instant.now())
-      ))
+            // Sets for other users
+            SlickTables.sets += Set(1, 4, 1, Instant.now(), None, None, 0),
+            SlickTables.sets += Set(2, 5, 2, Instant.now(), None, None, 0),
 
-    } yield ()
+            // Insert some repetitions
+            SlickTables.repetitions += Repetition(1, 0, 0),
+            SlickTables.repetitions += Repetition(2, 0, 0),
+            SlickTables.repetitions += Repetition(3, 0, 0),
 
-    setup.transform {
-      case Success(_) => Success(())
-      case Failure(ex) => Failure(new Exception("Database setup failed", ex))
+            // Insert some air qualities
+            SlickTables.airs += Air(0, 0, 0, 0, Instant.now()),
+            SlickTables.airs += Air(1, 3, 4, 5, Instant.now()),
+            SlickTables.airs += Air(2, 6, 7, 8, Instant.now())
+          )
+
+          db.run(insertData).transform {
+            case Success(_) => Success(())
+            case Failure(ex) => Failure(new Exception("Database setup and data insertion failed", ex))
+          }
+        } else {
+          Future.successful(())
+        }
+      }
     }
   }
 }
