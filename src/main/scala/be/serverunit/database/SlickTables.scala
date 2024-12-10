c4ee05d1c975e2271998efa0f32b90041499fdcb
Xantin <xantin@live.be>
2024-12-10 11:54:41 +0100

    Update stuff

--- src/main/scala/be/serverunit/database/SlickTables.scala
+++ src/main/scala/be/serverunit/database/SlickTables.scala
@@ -40,7 +40,7 @@ object SlickTables {
     def user = foreignKey("USER_FK", userID, users)(_.id)
   }
 
-  class Sets(tag: Tag) extends Table[UserSet](tag, "SETS") {
+  class Sets(tag: Tag) extends Table[UserSet](tag, "WORKOUT_SET") {
     def * = (id, sessionID, machineID, beginDate, endDate, repetitions, weight).mapTo[UserSet]
 
     def id = column[Long]("SET_ID", O.AutoInc, O.PrimaryKey)
